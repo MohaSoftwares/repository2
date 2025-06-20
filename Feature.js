9e487d6f92cfbc794374d3ff37ae018031bdb5ce
mohamed <teibani2005@email.com>
2025-06-20 18:10:24 +0100

    created Feature.js file that is a Letter finder code

---
+++ Feature.js
@@ -0,0 +1,12 @@
+function letterFinder (word,match){
+    for(let i = 0; i<word.length ; i++){
+        if (match == word[i]){
+            console.log("Found the ",word[i]," at ",i);
+        } else {
+            console.log("---no match found !")
+        };
+    };
+};
+
+letterFinder("mohamed","m");
+
